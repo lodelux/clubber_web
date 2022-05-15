64acad7025859d8b6e8ea258e07a55fefae817bf
lodelux <lodelux@live.it>
2022-05-15 11:21:36 +0200

    removed useless component

--- src/components/home/nights.vue
+++ src/components/home/nights.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="h-80 flex space-x-5 items-center px-5 overflow-scroll">
-    <night-card v-for="item in Array.from(Array(5).keys())" :key="item">
-    </night-card>
-  </div>
 </template>
 
 <script>
